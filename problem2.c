27ce610b72515f86e5527bba587c0cbd6f8e4fdf
Md Arif Uddin <97159618+MD-ARIF-UDDIN@users.noreply.github.com>
2022-11-01 22:20:43 +0600

    Create problem2.c
    Write a C program to take one positive integer N, the size of an array as input. Then take an integer array of size
    N as input and tell if the array contains only one unique value or not. Print “YES” or “NO”.

---
+++ problem2.c
@@ -0,0 +1,23 @@
+
+
+#include<stdio.h>
+int main(){
+int n,ar[40],count;
+scanf("%d",&n);
+for(int i=0;i<n;i++){
+scanf("%d",&ar[i]);
+}
+int a=ar[0];
+for(int i=1;i<n;i++){
+    if(a==ar[i]){
+       count++;
+    }
+    else
+        count=0;
+}
+if(count==0){
+    printf("no");
+}
+else
+    printf("yes");
+}
